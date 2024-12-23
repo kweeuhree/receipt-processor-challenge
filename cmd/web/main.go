ca530b6c5267e9fdd8cdd376eb17f841c0af0868
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-23 11:30:28 -0500

    feat: create main point of entry: - define application-wide dependencies; - initialize error and info logs; - initialize the application; - configure HTTP server; - listen and serve.

---
+++ cmd/web/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	"kweeuhree.receipt-processor-challenge/internal/models"
+)
+
+// Application-wide dependencies
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	// In-memory receipts storage
+	receiptStore *models.ReceiptStore
+}
+
+// Main point of entry
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	// Error and info logs
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Initialize the application with its dependencies
+	app := &application{
+		errorLog:     errorLog,
+		infoLog:      infoLog,
+		receiptStore: &models.ReceiptStore{},
+	}
+
+	// HTTP server config
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	// Listen and serve
+	infoLog.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
