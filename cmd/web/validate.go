94d68d35825566b7ec5326e8332926b590a0fda3
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-24 11:15:37 -0500

    feat: add validator model and validation helper logic

---
+++ cmd/web/validate.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"kweeuhree.receipt-processor-challenge/internal/validator"
+)
+
+func (input *ReceiptInput) Validate() {
+	input.CheckField(validator.NotBlank(input.Retailer), "retailerName", "This field cannot be blank")
+	input.CheckField(validator.NotBlank(input.PurchaseDate), "purchaseDate", "This field cannot be blank")
+	input.CheckField(validator.NotBlank(input.PurchaseTime), "purchaseTime", "This field cannot be blank")
+	input.CheckField(validator.NotBlank(input.Total), "total", "This field cannot be blank")
+	input.CheckField(validator.ItemsNotEmpty(input.Items), "items", "This field must have at least one object")
+}
---
+++ internal/validator/validator.go
@@ -0,0 +1,51 @@
+package validator
+
+import (
+	"strings"
+
+	"kweeuhree.receipt-processor-challenge/internal/models"
+)
+
+// Validator struct contains a map of validation errors
+type Validator struct {
+	NonFieldErrors []string
+	FieldErrors    map[string]string
+}
+
+// Returns true if the FieldErrors and nonFieldErrors map doesn't contain any entries
+func (v *Validator) Valid() bool {
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
+}
+
+// Adds an error message to the FieldErrors map
+func (v *Validator) AddFieldError(key, message string) {
+	// Initialize the map, if it isn't already initialized
+	if v.FieldErrors == nil {
+		v.FieldErrors = make(map[string]string)
+	}
+	if _, exists := v.FieldErrors[key]; !exists {
+		v.FieldErrors[key] = message
+	}
+}
+
+// Adds an error message to the FieldErrors map if a
+// validation check is not 'ok'
+func (v *Validator) CheckField(ok bool, key, message string) {
+	if !ok {
+		v.AddFieldError(key, message)
+	}
+}
+
+// Returns true if a value is not an empty string
+func NotBlank(value string) bool {
+	return strings.TrimSpace(value) != ""
+}
+
+// Returns true if a value is not an empty array
+func ItemsNotEmpty(items []models.Item) bool {
+	return len(items) > 0
+}
