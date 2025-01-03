6be4b1371337983908ecc1bf820091b839bb7f8b
Nika Kolesnikova <firstnamenika@gmail.com>
2025-01-03 09:59:59 -0500

    test: abstract testing items into a separate file to improve readability

---
+++ cmd/utils/test-receipt-items.go
@@ -0,0 +1,18 @@
+package utils
+
+import "kweeuhree.receipt-processor-challenge/internal/models"
+
+var MountainDewReceiptItems = []models.Item{
+	{ShortDescription: "Mountain Dew 12PK", Price: "6.49"},
+	{ShortDescription: "Emils Cheese Pizza", Price: "12.25"},
+	{ShortDescription: "Knorr Creamy Chicken", Price: "1.26"},
+	{ShortDescription: "Doritos Nacho Cheese", Price: "3.35"},
+	{ShortDescription: "   Klarbrunn 12-PK 12 FL OZ  ", Price: "12.00"},
+}
+
+var GatoradeReceiptItems = []models.Item{
+	{ShortDescription: "Gatorade", Price: "2.25"},
+	{ShortDescription: "Gatorade", Price: "2.25"},
+	{ShortDescription: "Gatorade", Price: "2.25"},
+	{ShortDescription: "Gatorade", Price: "2.25"},
+}
