9f075f57981b321846c447da141cac7270ae5923
Nika Kolesnikova <firstnamenika@gmail.com>
2025-01-05 13:15:01 -0500

    feat: add validation logic that ensures that each item price is a valid number

--- cmd/handlers/validate.go
+++ cmd/handlers/validate.go
@@ -15,4 +15,7 @@ func (input *ReceiptInput) Validate() {
 	input.CheckField(v.NotBlank(input.Total), "total", "This field cannot be blank")
 	input.CheckField(v.ValidNumber(input.Total), "total", "This field must be a valid number")
 	input.CheckField(v.ItemsNotEmpty(input.Items), "items", "This field must have at least one object")
+	for _, item := range input.Items {
+		input.CheckField(v.ValidNumber(item.Price), "items", "Each item price must be a valid number")
+	}
 }
